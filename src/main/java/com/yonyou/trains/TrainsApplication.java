61b2f5d23ebb0a2fa5150f4af7be6d0b02bf226d
gsj <gsjzhbj@sina.com>
2019-04-12 15:27:15 +0800

    spring boot 修改为war形式

--- src/main/java/com/yonyou/trains/TrainsApplication.java
+++ src/main/java/com/yonyou/trains/TrainsApplication.java
@@ -1,18 +1,21 @@
 package com.yonyou.trains;
 
 import com.yonyou.trains.utils.ResponseUtil;
+import javafx.application.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.Calendar;
 import java.util.Date;
 
 @SpringBootApplication
 @ServletComponentScan
-public class TrainsApplication {
+public class TrainsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         Logger logger= LoggerFactory.getLogger(ResponseUtil.PROJECT_NAME);
@@ -21,6 +24,12 @@ public class TrainsApplication {
         SpringApplication.run(TrainsApplication.class, args);
         logger.debug("项目成功启动，耗时{}毫秒。",Calendar.getInstance().getTime().getTime()-begin.getTime());
     }
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(TrainsApplication.class);
+    }
+
 
 }
 
